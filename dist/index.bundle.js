c4271d12f8b8c39c816b57fbc6b6e03546652f71
Douglas Melo <douglasmnegri@gmail.com>
2024-03-26 21:24:50 -0300

    first commit

---
+++ dist/index.bundle.js
@@ -0,0 +1,6 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+console.log("Does it work?");
+console.log("It works even if I don't run webpack?")
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log("Does it work?");
+console.log("It works even if I don't run webpack?")
